d2fa4a0267577fbd939a38867c2041eeb4cc4661
Trifle22 <xromchenkov99@gmail.com>
2021-02-12 20:24:24 +0300

    full done todoList

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,25 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-
-const todoData = [];
-
+let todoData = [];
+let todoDataJSON;
+localStorage.setItem = ('tasks', []);
 
 const render = function() {
   todoList.textContent = '';
   todoCompleted.textContent = '';
 
+  let todoDataLocalStorage = JSON.parse(localStorage.getItem('tasks'));
 
-  todoData.forEach(function(item,i) {
+  if (todoDataLocalStorage  === null) {
+    todoDataLocalStorage = [];
+  }
+
+  todoDataLocalStorage.forEach(function(item,i) {
+    if (item === null) {
+      return;
+    }
+
     const li = document.createElement('li');
     li.classList.add('todo-item');
     li.innerHTML = '<span class="text-todo">'+ item.value +'</span>' +
@@ -30,15 +39,21 @@ const render = function() {
     const todoCompletedButton = li.querySelector('.todo-complete');
     todoCompletedButton.addEventListener('click', function() {
       item.completed = !item.completed;
+      localStorage.tasks = JSON.stringify(todoDataLocalStorage);
       render();
     });
+
     const todoDeleteButton = li.querySelector('.todo-remove');
     todoDeleteButton.addEventListener('click', function() {
       delete todoData[i];
+      todoDataJSON = JSON.stringify(todoData);
+      localStorage.tasks = todoDataJSON;
       render();
     });
   });
+
 };
+
 
 todoControl.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -46,8 +61,10 @@ todoControl.addEventListener('submit', function(event) {
     const newTodo = {
       value: headerInput.value,
       completed: false
-    }; 
+    };
     todoData.push(newTodo);
+    todoDataJSON = JSON.stringify(todoData);
+    localStorage.tasks = todoDataJSON;
     render();
     headerInput.value = '';
   } else {
